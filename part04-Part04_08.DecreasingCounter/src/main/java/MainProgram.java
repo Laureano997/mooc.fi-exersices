9e20e155e79c1714e1de0e39edf93c6e1664ca56
Black&Whitecat <laureanomoroni1@gmail.com>
2023-05-14 01:03:54 -0300

    second commit

--- part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
+++ part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -3,7 +3,7 @@ public class MainProgram {
 
     public static void main(String[] args) {
         DecreasingCounter counter = new DecreasingCounter(10);
-
+        
         counter.printValue();
         counter.decrement();
         counter.decrement();
