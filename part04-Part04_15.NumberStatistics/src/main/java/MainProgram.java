abdcab793bafb9f977ee450f77bec31cb54537eb
Black&Whitecat <laureanomoroni1@gmail.com>
2023-05-14 22:00:29 -0300

    third commit

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -12,5 +12,34 @@ public class MainProgram {
         // correct order in the main program. First the object that tracks the total
         // sum, secondly the object that tracks the sum of even numbers, 
         // and lastly the one that tracks the sum of odd numbers!
+        Statistics numbers = new Statistics();
+        Statistics even = new Statistics();
+        Statistics odd = new Statistics();
+        
+        System.out.println("Enter numbers:" );
+        while(true){
+            
+            int number = scanner.nextInt();
+             
+            if (number == -1){ 
+                break;
+            }else{
+                 
+                numbers.addNumber(number);
+            
+                if(number % 2 == 0){
+                    even.addNumber(number);
+                }else{
+                    odd.addNumber(number);
+            }
+            
+             }
+        }    
+        System.out.println("Sum: " + numbers.sum());
+        System.out.println("Count: " + numbers.getCount());
+        System.out.println("Average: " + numbers.average());
+        System.out.println("Sum of even numbers: " + even.sum());
+        System.out.println("Sum of odd numbers: " + odd.sum());
     }
+    
 }
